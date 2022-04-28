7875c60a030df2d7e977a71df871f80930de7d29
MariaLuizaCw <marialuizacw@gmail.com>
2022-04-27 22:40:34 -0300

    feat: add Jacobi

--- library.py
+++ library.py
@@ -4,8 +4,8 @@ from turtle import forward
 
 class algcom:
     def __init__(self, n, icod, idet, A, b, tolm):
-        self.n, self.icod, self.idet, self.A, self.b, self.tolm = n, idet, icod, A, b, tolm
-
+        self.n, self.icod, self.idet, self.A, self.b, self.tolm = n, icod, idet, A, b, tolm
+        self.error = 0
 
     def _back_substitution(self, y):
         x = [0]*self.n
@@ -44,19 +44,77 @@ class algcom:
             for c in range(i, self.n):
                 for r in range(i+1, self.n):
                     if c == i:
-                        self.A[r][c] = self.A[r][c]/self.A[c][c]
+                        try:
+                            self.A[r][c] = self.A[r][c]/self.A[c][c]
+                        except:
+                            self.error = "Decomposição LU não é possível"
+                            return
                     else:
                         self.A[r][c] = self.A[r][c] - self.A[r][i] * self.A[i][c]
-        return self.A
+        return
     
     def Cholesky(self):
         for i in range(self.n):
-            self.A[i][i] = (self.A[i][i] - sum([self.A[i][k]**2  for k in range(0,  i)]))**0.5
+            raiz = self.A[i][i] - sum([self.A[i][k]**2  for k in range(0,  i)])
+            if raiz < 0:
+                self.error =  "Decomposição Cholesky não é possível"
+                return
+            else:
+                self.A[i][i] = (raiz)**0.5
             for j in range(i+1, self.n):
-                self.A[j][i] = (self.A[i][j] -  sum([self.A[i][k]*self.A[j][k]  for k in range(0,  i)]))/self.A[i][i]
+                try:
+                    self.A[j][i] = (self.A[i][j] -  sum([self.A[i][k]*self.A[j][k]  for k in range(0,  i)]))/self.A[i][i]
+                except:
+                    self.error = "Decomposição LU não é possível"
+                    return
                 
-        return self.A
+        return
     
+
+    def norma(self, x, y):
+        return (sum([(x[i] - y[i])**2 for i in range(len(x))])**0.5)/(sum([x[i]**2 for i in range(len(x))])**0.5)
+
+
+    def Jacobi(self):
+        x = [1]*self.n
+      
+        temp = x.copy()
+        last_res = []
+        while(True):
+            for i in range(self.n):
+                soma = 0
+                for j in range(self.n):
+                    if j != i:
+                        soma += self.A[i][j] * x[j]
+                temp[i] = (self.b[i] - soma)/self.A[i][i]
+
+            res = self.norma(temp, x)
+            last_res.append(res)
+            if len(last_res) >= 10:
+                diff_res = [last_res[i] - last_res[i-1]  for i in range(len(last_res) - 3, len(last_res))]
+                tendecy = sum(list(map(lambda x: x > 0, diff_res)))
+                if tendecy == 0:
+                    self.error = "Houve divergência no método Jacobi"
+                    return 
+
+            print(x, temp)
+            x = temp.copy()
+            if res <= self.tolm:
+                return x
+
+
+
+
+    # def gaussSeidel(self):
+    #     x = [1]*self.n
+    #     for i in range(self.n):
+    #         soma = 0
+    #         for j in range(self.n):
+    #             if j != i:
+    #                 soma += self.A[i][j] * x[j]
+    #         x[i] = (self.b[i] - soma)/self.A[i][i]
+             
+    #     return x
     def solve(self):
         if self.icod == 1:
             self._LU_decomposition()
@@ -64,33 +122,42 @@ class algcom:
             if self.idet != 0:
                 for x in range(0,self.n):
                     det *= self.A[x][x]
-            return self._back_substitution(self._foward_substitution(self.b)), det
+            if not self.error:
+                return self._back_substitution(self._foward_substitution(self.b)), det
+
         if self.icod == 2:
             self.Cholesky()
             det = 1 
             if self.idet != 0:
                 for x in range(0,self.n):
                     det *= self.A[x][x]
-            return  self._back_substitution(self._foward_substitution(self.b)), det**2                
+            if not self.error:
+                return self._back_substitution(self._foward_substitution(self.b)), det
+        if self.icod == 3:
+            if not self.error:
+                return self.Jacobi(), 1
     def output(self):
+        sol = self.solve() 
+        print(sol)
         with open("output.txt", "w") as arquivo:
-            if self.idet != 0:
-               sol = self.solve() 
-               arquivo.write(f"Solucao do sistema X: {sol[0]}\n Determinante: {sol[1]}\n")
-            else: 
-                arquivo.write(f"Solucao do sistema X: {sol[0]}\n")
-            
+            if not self.error:
+                if self.idet != 0:
+                    arquivo.write(f"Solucao do sistema X: {sol[0]}\n Determinante: {sol[1]}\n")
+                else: 
+                    arquivo.write(f"Solucao do sistema X: {sol[0]}\n")
+            else:
+                 arquivo.write(f"Error: {self.error}\n")
 
 # n = int(input('Qual o tamanho da matriz?'))
 # icod = int(input('Qual o código da operação?'))
-idet = int(input('Devo calcular a determinante?'))
-A = eval(input('Insira a matriz A:'))
+# idet = int(input('Devo calcular a determinante?'))
+# A = eval(input('Insira a matriz A:'))
 #y = eval(input('Insira o matriz y:'))
 
-b = eval(input('Insira o vetor b'))
+# b = eval(input('Insira o vetor b'))
 # tolm = int(input('Qual o número máximo de iterações?'))
 
-teste = algcom(3,2, idet, A,b, None)
+teste = algcom(3, 3, 0, [[1, 2, 3], [1, -2, -1], [2, -3, 4]],[20000, 20000, 20000], 10**(-3))
 
 teste.output()
 
